3f3f59060dee9c315287c1d155a7b615fa99c654
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-02-13 19:05:31 -0800

    setup first Link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "611e0bfdf8fe20b8"; }
+/******/ 		__webpack_require__.h = function() { return "80f5ba34446cda63"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
